cc38c1fadaa3cbd8447b7a86fe31c1c0c564a8c6
Simar111 <simarnarula1477@gmail.com>
2025-08-25 22:53:12 +0530

    done

---
+++ server/routes/serverRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const {
+  addStaff,
+  deleteStaff,
+  updateStaff,
+  getStaffs,
+  getStaffById
+} = require("../controllers/staffController");
+
+// Create new staff
+router.post("/add", addStaff);
+
+// Get all staff members
+router.get("/", getStaffs);
+
+// Get single staff by ID
+router.get("/:id", getStaffById);
+
+// Update staff by ID
+router.put("/:id", updateStaff);
+
+// Delete staff by ID
+router.delete("/:id", deleteStaff);
+
+module.exports = router;
