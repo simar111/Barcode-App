dbb23d6a71f51a71c805310c4885f647607e6914
Simar111 <simarnarula1477@gmail.com>
2025-08-25 22:40:14 +0530

    done

---
+++ server/models/adminModel.js
@@ -0,0 +1,22 @@
+// models/Admin.js
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Admin", adminSchema);
