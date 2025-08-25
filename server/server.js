0a177b041fe387bd2322344cb5705b5aebafa6fc
Simar111 <simarnarula1477@gmail.com>
2025-08-25 22:54:33 +0530

    done

--- server/server.js
+++ server/server.js
@@ -20,5 +20,7 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/login",loginRoutes);
+app.use("/api/admins", adminRoutes);
+app.use("/api/staffs", staffRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
