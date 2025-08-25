4d9b30e1d20b43203506e5cff09ddc56b7e74152
Simar111 <simarnarula1477@gmail.com>
2025-08-25 22:48:19 +0530

    done

--- server/server.js
+++ server/server.js
@@ -16,6 +16,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
-
+app.use("/api/login",loginRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
