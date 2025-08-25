4ea6431a896a331919a70f5970a233f81f41fd63
Simar111 <simarnarula1477@gmail.com>
2025-08-25 22:52:43 +0530

    done

---
+++ server/routes/adminRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const {
+  addAdmin,
+  deleteAdmin,
+  updateAdmin,
+  getAdmins,
+  getAdminById
+} = require("../controllers/adminController");
+
+// Create new admin
+router.post("/add", addAdmin);
+
+// Get all admins
+router.get("/", getAdmins);
+
+// Get single admin by ID
+router.get("/:id", getAdminById);
+
+// Update admin by ID
+router.put("/:id", updateAdmin);
+
+// Delete admin by ID
+router.delete("/:id", deleteAdmin);
+
+module.exports = router;
