248b29ad72c285eb8803bde60c35a164afaee7bf
Simar111 <simarnarula1477@gmail.com>
2025-08-25 22:40:39 +0530

    done

---
+++ server/models/staffModel.js
@@ -0,0 +1,22 @@
+// models/Staff.js
+const mongoose = require("mongoose");
+
+const staffSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Staff", staffSchema);
