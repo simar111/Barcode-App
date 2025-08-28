9d0ffc8d7738d7d4470c2092a3c836b8e5a558b3
harshit6002 <harshitemail2006@gmail.com>
2025-08-28 14:48:55 +0530

    d

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,8 @@
-// config/db.js
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://harshitmongodb:harshit14012006@cluster0.7xuteey.mongodb.net/posApp?retryWrites=true&w=majority&appName=Cluster0", {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
