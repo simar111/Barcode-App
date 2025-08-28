8512827453a97ee2f156a5f0e389ece0313a62f7
harshit6002 <harshitemail2006@gmail.com>
2025-08-28 16:30:41 +0530

    d

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/login", {
+      const res = await axios.post("https://pos-app-backend-61sv.onrender.com/api/login", {
         email,
         password,
       });
