b5fc26d8ed1db3d4c4f77b7e5b6dd0398c0c6a32
harshit6002 <harshitemail2006@gmail.com>
2025-08-29 22:07:12 +0530

    d

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://barcode-scanner-app-nejj.onrender.com/api/auth/login", {
         email,
         password,
       });
